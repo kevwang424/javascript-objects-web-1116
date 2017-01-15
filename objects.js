da8dcc7f970833d4011433b4e284f75749f057b5
kevwang424 <kev.wang424@gmail.com>
2017-01-15 17:08:06 -0500

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {adele: 'hello', beyonce: 'halo'}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+}
